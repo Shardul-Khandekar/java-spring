6f6ab2bc5dc15e802eabefbe86144c09b9994c4b
Shardul Khandekar <khandekarshardul@gmail.com>
2026-01-28 00:00:00 -0500

    differentiating between stack and heap memory

--- cloud/src/main/java/com/starter/cloud/HelloService.java
+++ cloud/src/main/java/com/starter/cloud/HelloService.java
@@ -5,11 +5,24 @@ import org.springframework.stereotype.Service;
 @Service
 public class HelloService {
 
+    // Global state, it will be a part of the heap -> shared space for all threads
+    private int counter = 0;
+
     public String getGreeting(String name){
         return "Hello " + name + " from the service layer!";
     }
 
     public String processUser(UserDTO user){
+        
+        counter++;
+        System.out.println("Processing user number: " + counter);
+
+        // Local state, will be part of private stack memory
+        int methodCounter = 0;
+        methodCounter++;
+
+        System.out.println("Method called " + methodCounter);
+
         return "User " + user.getName() + " age " + user.getAge() + " has been registered!";
     }
     
