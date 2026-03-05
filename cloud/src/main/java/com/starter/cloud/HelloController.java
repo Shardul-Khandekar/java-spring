734140e93bfd09384a290536f73e1347f94fecfd
Shardul Khandekar <khandekarshardul@gmail.com>
2026-03-04 22:57:29 -0500

    adding service layer

--- cloud/src/main/java/com/starter/cloud/HelloController.java
+++ cloud/src/main/java/com/starter/cloud/HelloController.java
@@ -1,5 +1,6 @@
 package com.starter.cloud;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,13 +8,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Autowired
+    private HelloService helloService;
+
     @GetMapping("/hello")
     public String sayHello() {
-        return "microservice-1 is online";
+        return helloService.getGreeting();
     }
-    
     
 }
 
 // RestController tells spring that this class will handle web requests
 // GetMapping tells spring that this method will be called when someone visits localhost:8080/hello
+// @Autowired finds the HelloService class and creates an instance, dependency injection
